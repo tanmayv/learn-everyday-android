b01ea55dd716aad7036d14d4d63cbb49ed986f6b
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2016-08-17 00:49:59 +0530

    More stable version

---
+++ app/src/main/java/com/tanmayvijayvargiya/factseveryday/event/FactsQueryEvent.java
@@ -0,0 +1,7 @@
+package com.tanmayvijayvargiya.factseveryday.event;
+
+/**
+ * Created by tanmayvijayvargiya on 16/08/16.
+ */
+public class FactsQueryEvent {
+}
---
+++ app/src/main/java/com/tanmayvijayvargiya/factseveryday/job/QueryFactsJob.java
@@ -0,0 +1,7 @@
+package com.tanmayvijayvargiya.factseveryday.job;
+
+/**
+ * Created by tanmayvijayvargiya on 16/08/16.
+ */
+public class QueryFactsJob {
+}
